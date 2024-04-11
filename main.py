3058f32085ac7d6e2bd788ee0df0898dec6c0835
AddChew <addison_chew@outlook.com>
2024-04-12 00:38:44 +0800

    add poc

--- main.py
+++ main.py
@@ -1,28 +1,69 @@
 from uuid import uuid4
 from typing import List
-from pydantic import UUID4, BaseModel, Field, model_validator
+from pydantic import UUID4, BaseModel, Field, model_serializer
 
 
-class Account(BaseModel):
+# Proposed method is to have two sets of models, one set with id fields, another set without
+# Set with id fields is used for actual prod
+# Set without id fields is used for testing evaluation purpose
+
+
+class IDMixin(BaseModel):
     id: UUID4 = Field(default_factory = uuid4)
+
+
+class AccountBase(BaseModel):
     acctno: int
     currency: str
+
+
+class Account(AccountBase, IDMixin):
     owner_id: UUID4 = None
 
 
-class Person(BaseModel):
-    id: UUID4 = Field(default_factory = uuid4)
+class CustomerBase(BaseModel):
     name: str
     cif: int
-    accounts: List[Account] = []
+    accounts: List[AccountBase]
 
-    @model_validator(mode = "after")
-    def add_owner_id(self) -> "Person":
+
+class Customer(CustomerBase, IDMixin):
+    accounts: List[Account]
+    company_id: UUID4 = None
+
+    @model_serializer
+    def serialize(self):
         for account in self.accounts:
             account.owner_id = self.id
-        return self
+        return self.__dict__
+    
 
-account = Account(acctno = 123, currency = "SGD")
-owner = Person(name = "bob", cif = 123, accounts = [account])
+class CompanyBase(BaseModel):
+    cif: int
+    regNo: str
+    customers: List[CustomerBase]
 
-print(owner.model_dump_json())
+
+class Company(CompanyBase, IDMixin):
+    customers: List[Customer]
+
+    @model_serializer
+    def serialize(self):
+        for customer in self.customers:
+            customer.company_id = self.id
+        return self.__dict__
+
+
+account1 = Account(acctno = 123, currency = "SGD")
+account2 = Account(acctno = 456, currency = "USD")
+
+customer = Customer(name = "bob", cif = 123, accounts = [account1, account2])
+# customer.accounts = [account1, account2]
+# customer.accounts.append(account2)
+
+company = Company(cif = 789, regNo = "123D", customers = [customer])
+# print(company.model_dump())
+print(company.model_dump_json(indent = 2))
+
+# with open("789.json", "w") as f:
+#     f.write(company.model_dump_json(indent = 2))
