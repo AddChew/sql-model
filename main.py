2b5df9eac1caf86d15ad575687bd9df5e1238d43
AddChew <addison_chew@outlook.com>
2024-04-14 16:24:46 +0800

    fin

--- main.py
+++ main.py
@@ -1,79 +1,106 @@
 from uuid import uuid4
 from typing import List
 from difflib import HtmlDiff
-from pydantic import UUID4, BaseModel, Field, model_serializer
+from pydantic import UUID4, BaseModel, Field, computed_field
 
 
-# Proposed method is to have two sets of models, one set with id fields, another set without
-# Set with id fields is used for actual prod
-# Set without id fields is used for testing evaluation purpose
+class BaseModel(BaseModel):
+    id: UUID4 = Field(default_factory = uuid4, frozen = True)
 
 
-class IDMixin(BaseModel):
-    id: UUID4 = Field(default_factory = uuid4)
-
-
-class AccountBase(BaseModel):
+class Account(BaseModel):
     acctno: int
     currency: str
+    owner_id: UUID4 = Field(default = None)
 
 
-class Account(AccountBase, IDMixin):
-    owner_id: UUID4 = None
-
-
-class CustomerBase(BaseModel):
+class Customer(BaseModel):
     name: str
     cif: int
-    accounts: List[AccountBase]
-
-
-class Customer(CustomerBase, IDMixin):
-    accounts: List[Account]
+    accounts_: List[Account] = Field(default = [], exclude = True, alias = "accounts", repr = False)
     company_id: UUID4 = None
 
-    @model_serializer
-    def serialize(self):
-        for account in self.accounts:
+    @computed_field
+    @property
+    def accounts(self) -> List[Account]:
+        for account in self.accounts_:
             account.owner_id = self.id
-        return self.__dict__
-    
+        return self.accounts_
 
-class CompanyBase(BaseModel):
+    @accounts.setter
+    def accounts(self, accounts: List[Account]):
+        self.accounts_ = accounts     
+
+
+class Company(BaseModel):
     cif: int
     regNo: str
-    customers: List[CustomerBase]
+    customers_: List[Customer] = Field(default = [], exclude = True, alias = "customers", repr = False)
 
-
-class Company(CompanyBase, IDMixin):
-    customers: List[Customer]
-
-    @model_serializer
-    def serialize(self):
-        for customer in self.customers:
+    @computed_field
+    @property
+    def customers(self) -> List[Customer]:
+        for customer in self.customers_:
             customer.company_id = self.id
-        return self.__dict__
+        return self.customers_
+    
+    @customers.setter
+    def customers(self, customers: List[Customer]):
+        self.customers_ = customers
 
 
+### Generate JSON
 account1 = Account(acctno = 123, currency = "SGD")
 account2 = Account(acctno = 456, currency = "USD")
 
-customer = Customer(name = "bob", cif = 123, accounts = [account1, account2])
-# customer.accounts = [account1, account2]
-# customer.accounts.append(account2)
+customer1 = Customer(name = "bob", cif = 123, accounts = [account1, account2])
+customer2 = Customer(name = "collin", cif = 456)
+customer3 = Customer(name = "dolly", cif = 789)
 
-company1 = Company(cif = 789, regNo = "123D", customers = [customer])
-company2 = Company(cif = 1011, regNo = "456F", customers = [customer])
+customer2.accounts.append(account1)
+customer3.accounts = [account2]
+
+company1 = Company(cif = 1011, regNo = "123D", customers = [customer1, customer2])
+company2 = Company(cif = 1213, regNo = "456F")
+company3 = Company(cif = 1516, regNo = "789H")
+
+company2.customers.append(customer3)
+company3.customers = [customer2]
+
 # print(company1.model_dump())
-print(company1.model_dump_json(indent = 2))
+# print(company1.model_dump_json(indent = 2))
+print(company1.model_dump_json(
+    indent = 2, 
+    exclude = {
+        "id": True,
+        "customers": {
+            "__all__": {
+                "id": True,
+                "company_id": True,
+                "accounts": {
+                    "__all__": {"id", "owner_id"}
+                }
+            }
+        }
+    })
+)
 
 # with open("789.json", "w") as f:
 #     f.write(company1.model_dump_json(indent = 2))
 
-html_diff = HtmlDiff().make_file(
-    str(company1.model_dump_json(indent = 2)).split(), 
-    str(company2.model_dump_json(indent = 2)).split()
-)
 
-with open("diff.html", "w") as f:
-    f.write(html_diff)
+### Compare JSON
+# html_diff = HtmlDiff().make_file(
+#     str(company1.model_dump_json(indent = 2)).split(), 
+#     str(company2.model_dump_json(indent = 2)).split()
+# )
+
+# with open("diff.html", "w") as f:
+#     f.write(html_diff)
+
+
+### Load JSON
+# with open("789.json", "r") as f:
+#     company = Company.model_validate_json(f.read())
+
+# print(company)
